f82407d1836d558e65c91a59ce0ad2b43ae4ae30
Zulfiqar Ali <151381405+Zulfiqar-AIi@users.noreply.github.com>
2024-12-15 21:57:43 +0000

    new change

--- JavascriptLearnAndEarn/Project01/script.js
+++ JavascriptLearnAndEarn/Project01/script.js
@@ -1,5 +1,7 @@
 const buttons = document.querySelectorAll('.button');
 const body = document.querySelector('body');
+const h1 = document.querySelectorAll('h1')
+const h2 = document.querySelectorAll('h2')
 
 buttons.forEach(function (button) {
     button.addEventListener('click', function (e) {
@@ -11,15 +13,22 @@ buttons.forEach(function (button) {
                 break;
             case 'white':
                 body.style.backgroundColor = 'white';
+                body.style.color = 'black';
+                // h2.style.color = 'black';
                 break;
             case 'blue':
                 body.style.backgroundColor = 'blue';
+                body.style.color = 'black';
+                // h2.style.color = 'black';
                 break;
             case 'yellow':
                 body.style.backgroundColor = 'yellow';
+                body.style.color = 'black';
+                // h2.style.color = 'black';
                 break;
             default:
                 body.style.backgroundColor = 'inherit'; // Reset to default
+                body.style.color = 'inherit';
         }
     });
 });
