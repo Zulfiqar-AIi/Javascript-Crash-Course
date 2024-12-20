a219b79273a5959aa7af1233aca4f3e9989b6755
Zulfiqar Ali <151381405+Zulfiqar-AIi@users.noreply.github.com>
2024-12-20 20:38:03 +0000

    New Project

--- JavascriptLearnAndEarn/Project02/script.js
+++ JavascriptLearnAndEarn/Project02/script.js
@@ -1,1 +1,30 @@
-console.log("Project02");
+const form = document.querySelector('form');
+// this usecase will give you empty
+// const height = parseInt(document.querySelector('#height').value)
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  const height = parseInt(document.querySelector('#height').value);
+  const weight = parseInt(document.querySelector('#weight').value);
+  const results = document.querySelector('#results');
+
+  if (height === '' || height < 0 || isNaN(height)) {
+    results.innerHTML = `Please give a valid height ${height}`;
+  } else if (weight === '' || weight < 0 || isNaN(weight)) {
+    results.innerHTML = `Please give a valid weight ${weight}`;
+  } else {
+    const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+    //show the result
+    results.innerHTML = `<span>${bmi}</span>`;
+    if (bmi < 18.6) {
+        console.log("You are Underweight like Talha Anjum");
+    } else if (18.6 < bmi > 24.9) {
+        console.log("You are Normal like me");
+    } else (bmi > 24.9) {
+        console.log("You are Overweight like Altaf Hussain");
+    }
+    
+}
+});
+
