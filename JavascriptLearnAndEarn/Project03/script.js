56c29a58b89fa6376c75406afaf8f0d75cd5d07a
Zulfiqar Ali <151381405+Zulfiqar-AIi@users.noreply.github.com>
2024-12-28 20:53:01 +0000

    project03 completed

--- JavascriptLearnAndEarn/Project03/script.js
+++ JavascriptLearnAndEarn/Project03/script.js
@@ -1,4 +1,3 @@
-
 let randomNumber = parseInt(Math.random() * 100 + 1);
 
 const submit = document.querySelector('#subt');
@@ -19,26 +18,32 @@ if (playGame) {
   submit.addEventListener('click', function (e) {
     e.preventDefault();
     const guess = parseInt(userInput.value);
-    console.log(guess);
+    console.log(`Random Number: ${randomNumber}`);
     validateGuess(guess);
   });
 }
 
 function validateGuess(guess) {
   if (isNaN(guess)) {
-    alert('PLease enter a valid number');
+    alert('Please enter a valid number');
   } else if (guess < 1) {
-    alert('PLease enter a number more than 1');
+    alert('Please enter a number greater than 1');
   } else if (guess > 100) {
-    alert('PLease enter a  number less than 100');
+    alert('Please enter a number less than 100');
   } else {
     prevGuess.push(guess);
+    numGuess++;
+    displayGuess(guess);
+
+    // Check if it's the 10th guess
     if (numGuess === 11) {
-      displayGuess(guess);
-      displayMessage(`Game Over. Random number was ${randomNumber}`);
+      if (guess === randomNumber) {
+        displayMessage(`You guessed it right on your ${numGuess-1} attempt`);
+      } else {
+        displayMessage(`Game Over. Random number was ${randomNumber}`);
+      }
       endGame();
     } else {
-      displayGuess(guess);
       checkGuess(guess);
     }
   }
@@ -46,20 +51,19 @@ function validateGuess(guess) {
 
 function checkGuess(guess) {
   if (guess === randomNumber) {
-    displayMessage(`You guessed it right`);
+    displayMessage(`You guessed it right on your ${numGuess-1} attempt`);
     endGame();
   } else if (guess < randomNumber) {
-    displayMessage(`Number is TOOO low`);
+    displayMessage(`Number is TOO low`);
   } else if (guess > randomNumber) {
-    displayMessage(`Number is TOOO High`);
+    displayMessage(`Number is TOO high`);
   }
 }
 
 function displayGuess(guess) {
   userInput.value = '';
   guessSlot.innerHTML += `${guess}, `;
-  numGuess++;
-  remaining.innerHTML = `${12 - numGuess} `;
+  remaining.innerHTML = `${11 - numGuess}`;
 }
 
 function displayMessage(message) {
@@ -83,11 +87,10 @@ function newGame() {
     prevGuess = [];
     numGuess = 1;
     guessSlot.innerHTML = '';
-    remaining.innerHTML = `${12 - numGuess} `;
+    remaining.innerHTML = `${11 - numGuess}`;
     userInput.removeAttribute('disabled');
     startOver.removeChild(p);
 
     playGame = true;
   });
 }
-
